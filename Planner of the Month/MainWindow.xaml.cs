cde23544c48aadfb59d099b4118a9468b310de03
Михаил <makolin@bk.ru>
2020-10-14 19:28:04 +0500

    Правки

--- Planner of the Month/MainWindow.xaml.cs
+++ Planner of the Month/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,21 +13,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace Planner_of_the_Month
 {
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Task> GetData()
+        {
+            ObservableCollection<Task> taskdata = null; ;
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                taskdata = new ObservableCollection<Task>(db.Tasks.ToList());
+            }
+
+            return taskdata;
+        }
         public MainWindow()
         {
             InitializeComponent();
+            ObservableCollection<Task> taskdata = GetData();
+            TasksView.DataContext = taskdata;
         }
 
-        private void TasksView_Loaded(object sender, RoutedEventArgs e)
+        /*private void TasksView_Loaded(object sender, RoutedEventArgs e)
         {
             using (ApplicationContext db = new ApplicationContext())
             {
-                /*var Task = new Task
+                var Task = new Task
                 {
                     Name = "Администрирование и поддержка пользователей \"1С:УПП\"",
                     Category = CategoryTask.АСУП,
@@ -35,12 +49,12 @@ namespace Planner_of_the_Month
                 };
 
                 db.Tasks.Add(Task);
-                db.SaveChanges();*/
+                db.SaveChanges();
 
                 var tasks = db.Tasks.ToList();
                 TasksView.ItemsSource = tasks;
             }
-        }
+        }*/
 
         private void TasksView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
@@ -56,8 +70,16 @@ namespace Planner_of_the_Month
 
         private void DeleteTask_Click(object sender, RoutedEventArgs e)
         {
-            var selectIndex = TasksView.SelectedIndex;
-            var row = 
+            var find = TasksView.SelectedIndex;
+            if (find != -1)
+            {
+
+                TasksView.Items.Remove(TasksView.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать хотя бы одну строку!");
+            }
         }
     }
 }
